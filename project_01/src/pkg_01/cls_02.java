bd2d3491f5f26b771a8278ca1f692c0d2c1b5e93
JSC <JSC@DESKTOP-Q0PCB7C>
2023-10-16 11:23:30 -0700

    testing: first commit and push

---
+++ project_01/src/pkg_01/cls_02.java
@@ -0,0 +1,33 @@
+package pkg_01;
+
+public class cls_02 {
+	
+	public int add(int a, int b) {
+		int c = a+b;
+		return c;
+	}	
+	public int minus(int y, int z) {
+		int x = y-z;
+		return x;
+	}
+	public int multi(int q, int r) {
+		int p = q*r;
+		return p;
+	}
+	public void div(int n, int o) {
+		int m=n/o;
+		System.out.println("final result is " + m);
+	}
+	
+	public static void main(String[] args) {
+		cls_02 obj = new cls_02();
+		int sum1 = obj.add(10, 2);
+		int sum2 = obj.add(sum1, 2);
+		int sub = obj.minus(sum2, 2);
+		int mul = obj.multi(sub, 2);
+		obj.div(mul, 2);		
+		
+		
+	}
+
+}
